8f2ef20c003ad78c1b9fc36ed894db7362042bd4
VladimirKalinushkin <ya-vladimir-93@yandex.ru>
2023-11-24 16:45:54 +0300

    Реализация тонкой настройки вынесена в главное меню (логика  перенесена 'Как есть' из Сеттингс в главное меню. Это привводи к порядку в Сеттингс: сеттеры и геттеры режимов отдельно, соответственно сеттеры и геттеры имен - отдельно

--- Main_menu/main_menu.cpp
+++ Main_menu/main_menu.cpp
@@ -9,7 +9,7 @@ void main_menu(Settings &Main_settings, TokenStream &Stream) {
 
         try {
 
-            if(Main_settings.promt_must_otput())
+            if(Main_settings.promt_must_be_out())
                 cout << PROMT;
 
             if(Main_settings.get_mode_input() == Modes_input::console && check_exit_simbol(Stream)) 
@@ -33,16 +33,6 @@ void main_menu(Settings &Main_settings, TokenStream &Stream) {
     }
 } 
 
-bool check_exit_simbol(TokenStream &Stream) {
-
-    Token oper = Stream.get();
-    if(oper.type ==  Type_lexeme::exit_simbol)
-        return true;
-    
-    Stream.putback(oper);
-    return false;
-
-}
 bool enable_Main_modes(Settings &Main_settings, TokenStream &Stream) {
     
     Token oper = Stream.get();
@@ -52,7 +42,7 @@ bool enable_Main_modes(Settings &Main_settings, TokenStream &Stream) {
         return true; 
     }
     else if(oper.type == Type_lexeme::settings) {
-        Main_settings.main_menu_to_set_all_settings();
+        menu_to_set_Main_settings(Main_settings);
         return true;
     }
     else if(oper.type == Type_lexeme::key_word && oper.word == "from_file") {
@@ -65,6 +55,145 @@ bool enable_Main_modes(Settings &Main_settings, TokenStream &Stream) {
     return false;
 
 }
+bool check_exit_simbol(TokenStream &Stream) {
+
+    Token oper = Stream.get();
+    if(oper.type ==  Type_lexeme::exit_simbol)
+        return true;
+    
+    Stream.putback(oper);
+    return false;
+
+}
+
+
+void menu_to_set_Main_settings(Settings &Main_settings) {
+    
+    while (cin) {
+
+        cout << "Для настройки режима вычислений введите c, "
+             << "для настройки вывода файла - o, "
+             << "для режима вывода при вводе из файла - f, "
+             << "чтобы указать имя файла для вывода - n, "
+             << "для выхода - e: "
+             << '\n'
+             << PROMT;
+             
+        char ch;
+        cin >> ch;
+
+        if (ch =='e')
+            return;
+
+        switch (ch)
+        {
+        case 'c':
+            __set_mode_calculating(Main_settings);
+            break;
+        case 'o':
+            __set_mode_output(Main_settings);
+            break;
+        case 'f':
+            __set_mode_output_with_file(Main_settings);
+            break;
+        case 'n':
+            __set_name_file_to_output(Main_settings);
+            break;
+        default:
+            cout << "Неправильный ввод!\n";
+            return;
+            break;
+        }
+
+    }
+
+}
+
+void __set_mode_calculating(Settings &Main_settings)
+{
+    
+    cout << "Введите режим работы вычислений (полный режим с арабскими числами или ограниченный - с римскими): " 
+         << char(Modes_calculating::arabian) << " / " << char (Modes_calculating::roman) 
+         << '\n' << PROMT;
+    
+    char mode;
+    cin >> mode;
+
+    switch(mode){
+
+    case Modes_calculating::arabian:
+    case Modes_calculating::roman:
+        Main_settings.set_mode_calculating(Modes_calculating(mode));
+        break;
+
+    default:
+        throw MainException("Неправильный ввод!\n");
+
+    };
+
+}
+void __set_mode_output(Settings &Main_settings) {
+
+    cout << "Введите режим вывода (только в консоль, в консоль и в файл, или только в файл): " 
+         << char(Modes_output::to_console) << " / " << char (Modes_output::to_console_and_file) << " / " << char(Modes_output::to_file)
+         << '\n' << PROMT;
+    
+    char mode;
+    cin >> mode;
+
+    switch(mode){
+
+    case Modes_output::to_console:
+    case Modes_output::to_console_and_file:
+    case Modes_output::to_file:
+        Main_settings.set_mode_output(Modes_output(mode));
+        return;
+        break;
+
+    default:
+        throw MainException("Неправильный ввод!\n");
+
+    };
+
+}
+void __set_mode_output_with_file(Settings &Main_settings) {
+
+    cout << "Введите режим вывода при вводе из файла (в консоль и файл или только в файл): " 
+         << char(Modes_output_with_file::on_to_console_and_file) << " / " << char (Modes_output_with_file::on_to_file)
+         << '\n' << PROMT;
+    
+    char mode;
+    cin >> mode;
+
+    switch(mode){
+
+    case Modes_output_with_file::on_to_console_and_file:
+    case Modes_output_with_file::on_to_file:
+        Main_settings.set_mode_output_with_file(Modes_output_with_file(mode));
+        return;
+        break;
+
+    default:
+        throw MainException("Неправильный ввод!\n");
+
+    };
+
+}
+void __set_name_file_to_output(Settings &Main_settings) {
+
+    string name;
+    cout    << "Введите имя файла. \n" 
+            << "Установлено - "
+            << Main_settings.get_name_file_to_output()
+            << "\n"
+            << PROMT;
+    cin >> name;
+
+    Main_settings.set_name_file_to_output(name);
+
+
+}
+
 
 void set_filestream_to_input(Settings &Main_settings) {
 
